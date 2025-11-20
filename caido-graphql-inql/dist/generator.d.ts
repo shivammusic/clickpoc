9d13175cfdcd388fedf5248245e989fb1cfacd2d
Shivam <41016630+shivammusic@users.noreply.github.com>
2025-11-20 14:41:42 -0500

    Delete caido-graphql-inql directory

--- caido-graphql-inql/dist/generator.d.ts
+++
@@ -1,3 +0,0 @@
-import { SchemaSummary } from "./schema";
-export declare function buildExampleQuery(schema: SchemaSummary, fieldName: string): string;
-export declare function buildExampleMutation(schema: SchemaSummary, fieldName: string): string;
